68ba76b6f96874b2f2ec8534e49108016da6b011
ForrestW <fwalsh@bgsu.edu>
2020-02-22 23:11:45 -0500

    Fixed multiple issues

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { cloneElement } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 // Map class
 // Should essentially be the element that holds all the cells.
-class Map extends React.Component{
+class Map extends React.Component
+{
+
+  
+  constructor()
+  {
+    // Gridsize variable
+    super();
+    this.gridSize = 8;
+
+  }
+  
+  getGridSize()
+  {
+    return this._gridSize;
+  }
 
   render()
   {
-    <body></body>
+    return <body>stuff</body>
+  };
+
+  CreateGrid()
+  {
+    // Array of cells
+    var theseGuys= new Array(this.gridSize)
+
+    for(var i = 0; i < this.gridSize; i++)
+    {
+      theseGuys[i] = new Cell;
+      theseGuys[i].render();
+    }
   };
 
 }
@@ -17,38 +44,34 @@ class Map extends React.Component{
 // Classes for each cell block on the grid
 class Cell extends React.Component
 {
+  /*
+  constructor()
+  {
+    // Determines if the tiles is a bomb or not, can be a int value to determine if close to a bomb
+    //this.isBomb = 0;
+  }*/
+
     render()
     {
-      <div></div>
+      return( <div>This Guy</div>)
     };
 }
 
-// Create Grid Function
-function CreateGrid()
-{
-    
-}
 
 
 // This is essentially a Main in C++?
 function App() {
+
+  var Board = new Map(8);
+
+  //Cell newCell = new Cell
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>This guy33333</div>,
+    Board.CreateGrid()
+
+    
   );
 }
 
