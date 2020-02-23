f004681b342d6ad71bc06b1563a642a3cba7fb4b
ForrestW <fwalsh@bgsu.edu>
2020-02-23 02:01:23 -0500

    Added more functions

--- MakeFileMaker/Maker.py
+++ MakeFileMaker/Maker.py
@@ -2,37 +2,45 @@
 
 import sys
 
-
 # Display arguments 
 print "Number of arguments :" + str(len(sys.argv)) + " arguments"
 print "Arguments: " + str(sys.argv)
 
 # Variables
 targetFile = sys.argv[1]
-
+additionalFiles = []
+numberOfFiles = len(sys.argv) - 1
 
 
 # Create new file in current directory
 def createMakefile():
     f = open("makefile", "w+")
-
     
 # Function that checks for extension
+# Skip a line function
+def skipLine():
+    f.write("\n")
 
+# Function that puts all files in additonal files list
+def getAdditionalFiles():
+    for x in range(0, numberOfFiles):
+        print()
 
 # Function that writes the basics of the makefile
 def universalOut():
     f.wriet("nothing")
 
 # Function that writes clean to file
-def macClean(language):
-    f.write("clean:\n")
-    f.write("     rm *.dll\n")
+def macClean():
+    f.write("\n clean:\n")
+    f.write("     rm *.")
     f.write("     rm *.")
     
 # Function that writes clean to file
 def winClean():
-    f.write()
+    f.write("\n clean:\n")
+    f.write("     del *.")
+    f.write("     del *.")
 
 # Function that gets the language
 def getLanguage():
