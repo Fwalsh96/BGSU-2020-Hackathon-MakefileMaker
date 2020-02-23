39b8debb566864147d6175118e3c5de04b3abb79
ForrestW <fwalsh@bgsu.edu>
2020-02-23 01:31:05 -0500

    Added more functions

--- MakeFileMaker/Maker.py
+++ MakeFileMaker/Maker.py
@@ -1,10 +1,48 @@
-# Program that formats files
+# Program that formats makefiles
 
-import sys   
+import sys
 
 
-print "Number of arguments :" len(sys.argv), " arguments"
-print "Arguments: " str(sys.argv)
+# Display arguments 
+print "Number of arguments :" + str(len(sys.argv)) + " arguments"
+print "Arguments: " + str(sys.argv)
+
+# Variables
+targetFile = sys.argv[1]
+
+
+
+# Create new file in current directory
+def createMakefile():
+    f = open("makefile", "w+")
+
+    
+# Function that checks for extension
+
+
+# Function that writes the basics of the makefile
+def universalOut():
+    f.wriet("nothing")
+
+# Function that writes clean to file
+def macClean(language):
+    f.write("clean:\n")
+    f.write("     rm *.dll\n")
+    f.write("     rm *.")
+    
+# Function that writes clean to file
+def winClean():
+    f.write()
+
+# Function that gets the language
+def getLanguage():
+    return
+
+# Function that writes the target file, and the included parameters
+def target():
+    f.write(repr(targetFile) + ':\n')
+    
+
 
 
 
