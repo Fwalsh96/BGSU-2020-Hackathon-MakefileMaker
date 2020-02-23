8692d60a056626f4fd1cf20ffcea0ec990e23583
Adam Baty <adambaty100@gmail.com>
2020-02-22 21:28:31 -0500

    Fix Stuff

--- minesweeper/src/App.js
+++ minesweeper/src/App.js
@@ -1,92 +1,26 @@
 import React from 'react';
-import logo, { ReactComponent } from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-
-        <div class="grid-container">
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-          <div></div>
-          <div></div>  
-          <div></div>
-</div>
-        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
-class board extends React.Component{
-
-}
-
 export default App;
-
